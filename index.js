3a007be4029a418c1b02219b2c0068754659f437
shanelonergan <sptlonergan@gmail.com>
2019-12-20 12:18:10 -0500

    1 file modified

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-const button = document.createElement('button')
-button.innerHTML = "<button type='button'> Click me! </button>" 
+const button = document.createElement('div')
+button.innerHTML = "<button type='button' style='color:green;border-color:red;width:15vw;height:5vh;font-weight:bold;font-size:2.5vw'> Click me! </button>" 
 appDiv.appendChild(button)
+
+const makeButton = () => {
+  const button = document.createElement('div')
+  button.innerHTML = "<button type='button' style='color:green;border-color:red;width:15vw;height:5vh;font-weight:bold;font-size:2.5vw'> Click me! </button>" 
+  appDiv.appendChild(button)
+}
+
+button.addEventListener
