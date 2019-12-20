10b9d72b79ce88bd77395e55676f7eb59cf7d50c
shanelonergan <sptlonergan@gmail.com>
2019-12-20 12:25:11 -0500

    1 file modified

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ const makeButton = () => {
   const randomBorderColor = Math.floor(Math.random()*16777215).toString(16);
   const randomTextColor = Math.floor(Math.random()*16777215).toString(16);
 
-  button.innerHTML = `<button type='button' style='color:green;border-color:red;width:15vw;height:5vh;font-weight:bold;font-size:2.5vw;margin:1vh'> Click me! </button>`
+  button.innerHTML = `<button type='button' style='color:${randomTextColor};border-color:${randomBorderColor};width:15vw;height:5vh;font-weight:bold;font-size:2.5vw;margin:1vh'> Click me! </button>`
+
   appDiv.appendChild(button)
+
   button.addEventListener('click', makeButton)
 }
 
